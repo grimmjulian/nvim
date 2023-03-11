1a7ba7e43baabfb1fc1395ac2c7dadbbf68d0ead
Julian Grimm <grimmjulian225@gmail.com>
2023-03-11 09:46:37 +0100

    Moved remaps for plugin commands to after/plugin

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -10,3 +10,21 @@ telescope.setup {
     },
   },
 }
+
+local wk = require("which-key")
+local tb = require("telescope.builtin")
+
+wk.register({
+  ["<leader>f"] = {
+    name = "find",
+    f = { tb.find_files, "Find File" },
+    b = { tb.buffers, "Find Buffers" },
+    g = { function() tb.grep_string({ search = vim.fn.input("Grep > ") }) end, "Find Grep String" },
+    h = { tb.help_tags, "Find Help Tags" },
+    d = { tb.diagnostics, "Find diagnostics" },
+    t = { "<cmd>TodoTelescope<cr>", "Find todo comments" }
+  },
+  ["<leader>b"] = {
+    { tb.buffers, "Buffers"}
+}})
+
--- lua/julian/remap.lua
+++ lua/julian/remap.lua
@@ -1,31 +1,17 @@
 local wk = require("which-key")
-local tb = require("telescope.builtin")
 
+-- Here are only general Keymaps found that do not require any plugin
+-- Plugin Keymaps are found in the corresponding after/plugin file.
 -- To open Telescope results in Trouble, use <c-t>
 -- This is configed in after/plugin/telesecope.lua
 
 wk.register({
   ["<leader>e"] = { vim.cmd.Ex, "Open Filebrowser" },
-  ["<leader>f"] = {
-    name = "find",
-    f = { tb.find_files, "Find File" },
-    b = { tb.buffers, "Find Buffers" },
-    g = { function() tb.grep_string({ search = vim.fn.input("Grep > ") }) end, "Find Grep String" },
-    h = { tb.help_tags, "Find Help Tags" },
-    d = { tb.diagnostics, "Find diagnostics" },
-    t = { "<cmd>TodoTelescope<cr>", "Find todo comments" }
-  },
   ["<leader>t"] = {
     name = "open terminal",
     t = { "<c-w><c-n><cmd>terminal<cr>A", "Open Terminal in new window" },
     -- TODO: Filetype specifics should be correctly setup
     rr = { "<c-w><c-n><cmd>terminal<cr>AR<cr>", "Open Terminal with R open" }
-  },
-  ["<leader>b"] = {
-    { tb.buffers, "Buffers"}
-  },
-  ["<leader>g"] = {
-    { "<cmd>Git<cr>", "Git"}
   },
   ["<c-n><c-n>"] = {
     "<c-\\><c-n>",
