c24c092dbcf093be612e8e125461d4723d4bec91
Julian Grimm <grimmjulian225@gmail.com>
2023-11-11 00:41:47 +0100

    Initial Commit

---
+++ after/plugin/mini.lua
@@ -0,0 +1,6 @@
+-- require("mini.animate").setup()
+require("mini.surround").setup()
+require("mini.pairs").setup()
+require("mini.hipatterns").setup()
+require("mini.statusline").setup()
+require("mini.trailspace").setup()
